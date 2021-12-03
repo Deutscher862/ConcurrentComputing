ef4ce9fa5ab4629d4fd778498a59bd470049f8b8
Adam <adamniemiec862@gmail.com>
2021-12-03 14:49:57 +0100

    Add average result counting

--- src/main/java/com/agh/lab8/Main.java
+++ src/main/java/com/agh/lab8/Main.java
@@ -20,29 +20,35 @@ class Main {
         try (Writer output = new BufferedWriter(new FileWriter("results.txt", true))) {
             for (int no_threads = 1; no_threads <= 100; no_threads++) {
                 System.out.println(no_threads);
-                List<Future<Double>> futures = new ArrayList<>();
+                double[] values = {0, 0, 0, 0};
+                for (int j = 0; j < 100; j++) {
+                    List<Future<Double>> futures = new ArrayList<>();
+                    ExecutorService executor = Executors.newFixedThreadPool(4);
+                    futures.add(executor.submit(new NewSingleThreadExecutorTest(MAX_ITER, no_threads)));
+                    futures.add(executor.submit(new ThreadPoolTest(MAX_ITER, no_threads, Executors.newFixedThreadPool(no_threads))));
+                    futures.add(executor.submit(new ThreadPoolTest(MAX_ITER, no_threads, Executors.newCachedThreadPool())));
+                    futures.add(executor.submit(new ThreadPoolTest(MAX_ITER, no_threads, Executors.newWorkStealingPool(no_threads))));
 
-                ExecutorService executor = Executors.newFixedThreadPool(4);
-                futures.add(executor.submit(new NewSingleThreadExecutorTest(MAX_ITER, no_threads)));
-                futures.add(executor.submit(new ThreadPoolTest(MAX_ITER, no_threads, Executors.newFixedThreadPool(no_threads))));
-                futures.add(executor.submit(new ThreadPoolTest(MAX_ITER, no_threads, Executors.newCachedThreadPool())));
-                futures.add(executor.submit(new ThreadPoolTest(MAX_ITER, no_threads, Executors.newWorkStealingPool(no_threads))));
+                    executor.shutdown();
 
-                executor.shutdown();
+                    boolean executed = false;
+                    try {
+                        while (!executed)
+                            if (executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS))
+                                executed = true;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
-                boolean executed = false;
-                try {
-                    while (!executed)
-                        if (executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS))
-                            executed = true;
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    for (int k = 0; k < 4; k++) {
+                        values[k] += futures.get(k).get();
+                    }
                 }
 
                 output.append(String.valueOf(no_threads)).append(" ");
 
-                for (Future<Double> future : futures) {
-                    output.append(String.valueOf(future.get())).append(" ");
+                for (int k = 0; k < 4; k++) {
+                    output.append(String.valueOf(values[k] / 100.0)).append(" ");
                 }
                 output.append("\n");
             }
@@ -122,9 +128,7 @@ class ThreadPoolTest extends ExecutorTest {
 }
 
 class Mandelbrot extends JFrame {
-    private final double ZOOM = 150;
     private final BufferedImage I;
-    private double zx, zy, cX, cY, tmp;
     private final int MAX_ITER;
 
     Mandelbrot(int MAX_ITER) {
@@ -139,12 +143,14 @@ class Mandelbrot extends JFrame {
     void count(int first_x, int step) {
         for (int y = 0; y < getHeight(); y++) {
             for (int x = first_x; x < getWidth(); x += step) {
-                zx = zy = 0;
-                cX = (x - 400) / ZOOM;
-                cY = (y - 300) / ZOOM;
+                double zy;
+                double zx = zy = 0;
+                double ZOOM = 150;
+                double cX = (x - 400) / ZOOM;
+                double cY = (y - 300) / ZOOM;
                 int iter = MAX_ITER;
                 while (zx * zx + zy * zy < 4 && iter > 0) {
-                    tmp = zx * zx - zy * zy + cX;
+                    double tmp = zx * zx - zy * zy + cX;
                     zy = 2.0 * zx * zy + cY;
                     zx = tmp;
                     iter--;
