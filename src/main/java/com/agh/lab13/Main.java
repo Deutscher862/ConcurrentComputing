915406651cc9581945490e929d212802fd4c8a21
Adam <adamniemiec862@gmail.com>
2022-01-13 16:08:21 +0100

    Add disorder consumer reading

--- src/main/java/com/agh/lab13/Main.java
+++ src/main/java/com/agh/lab13/Main.java
@@ -4,17 +4,19 @@ import org.jcsp.lang.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public final class Main {
     public static void main(String[] args) {
-        final One2OneChannelInt prodChan = Channel.one2oneInt();
-        final One2OneChannelInt consReq = Channel.one2oneInt();
-        final One2OneChannelInt consChan = Channel.one2oneInt();
+        int size = 3;
+        One2OneChannelInt prodChan = Channel.one2oneInt();
+        One2OneChannelInt consReq = Channel.one2oneInt();
+        One2OneChannelInt consChan = Channel.one2oneInt();
 
         CSProcess[] procList = {
-                new Producer(prodChan, 3),
+                new Producer(prodChan, 5),
                 new Consumer(consReq, consChan),
-                new Buffer(prodChan, consReq, consChan)
+                new DisorderBuffer(prodChan, consReq, consChan, size)
         };
         Parallel par = new Parallel(procList);
         par.run();
@@ -62,24 +64,31 @@ class Consumer implements CSProcess {
                 break;
             }
             System.out.println("Czytam: " + item);
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         System.out.println("Konsument konczy prace");
     }
 }
 
-class Buffer implements CSProcess {
+class OrderBuffer implements CSProcess {
     private final One2OneChannelInt producerInput;
     private final One2OneChannelInt consumerDataRequest;
     private final One2OneChannelInt consumerOutput;
     private final List<Integer> buffer = new ArrayList<>();
-    private final int size = 10;
+    private final int size;
 
-    Buffer(One2OneChannelInt in, One2OneChannelInt req, One2OneChannelInt out) {
+    OrderBuffer(One2OneChannelInt in, One2OneChannelInt req, One2OneChannelInt out, int size) {
         this.producerInput = in;
         this.consumerDataRequest = req;
         this.consumerOutput = out;
+        this.size = size;
     }
 
+    @Override
     public void run() {
         final Guard[] guards = {producerInput.in(), consumerDataRequest.in()};
         final Alternative alt = new Alternative(guards);
@@ -102,6 +111,74 @@ class Buffer implements CSProcess {
                     int item = buffer.remove(0);
                     if (item == -1)
                         countdown--;
+                    consumerOutput.out().write(item);
+                }
+            }
+        }
+        System.out.println("Bufor konczy prace");
+    }
+}
+
+class DisorderBuffer implements CSProcess {
+    private final One2OneChannelInt producerInput;
+    private final One2OneChannelInt consumerDataRequest;
+    private final One2OneChannelInt consumerOutput;
+    private final int[] buffer;
+    private final int maxSize;
+    private int currentSize = 0;
+
+    DisorderBuffer(One2OneChannelInt in, One2OneChannelInt req, One2OneChannelInt out, int size) {
+        this.producerInput = in;
+        this.consumerDataRequest = req;
+        this.consumerOutput = out;
+        this.maxSize = size;
+        this.buffer = new int[size];
+        for (int i = 0; i < size; i++) {
+            this.buffer[i] = 0;
+        }
+    }
+
+    public void run() {
+        final Guard[] guards = {producerInput.in(), consumerDataRequest.in()};
+        final Alternative alt = new Alternative(guards);
+        int countdown = 2;
+        while (countdown > 0) {
+            int index = alt.fairSelect();
+            if (index == 0 && currentSize < maxSize) {
+                int item = producerInput.in().read();
+                if (item < 0) {
+                    countdown--;
+                    item = -1;
+                }
+                int pointer = 0;
+                while (pointer < maxSize && buffer[pointer] != 0) {
+                    pointer++;
+                }
+                buffer[pointer] = item;
+                currentSize++;
+            } else {
+                consumerDataRequest.in().read();
+                if (currentSize == 0) {
+                    consumerOutput.out().write(0);
+                } else {
+                    int pointer = 0;
+                    while (pointer < maxSize && buffer[pointer] == 0) {
+                        pointer++;
+                    }
+                    int item = buffer[pointer];
+                    if (item == -1 && currentSize > 1) {
+                        pointer++;
+                        while (pointer < maxSize && buffer[pointer] == 0) {
+                            pointer++;
+                        }
+                    }
+                    if (pointer < maxSize) {
+                        item = buffer[pointer];
+                        buffer[pointer] = 0;
+                        currentSize--;
+                    } else {
+                        countdown--;
+                    }
                     consumerOutput.out().write(item);
                 }
             }
